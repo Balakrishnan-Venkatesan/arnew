34c5ae1177d14a56b16800a65df72cc5811d9103
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-11-29 14:25:57 -0500

    dbconn namespace

--- dbConn.php
+++ dbConn.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace classes;
+
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
@@ -7,15 +9,16 @@ define('DATABASE', 'bv98');
 define('USERNAME', 'bv98');
 define('PASSWORD', 'dvJjpozdZ');
 define('CONNECTION', 'sql1.njit.edu');
+
 class dbConn{
     protected static $db;
     private function __construct() {
         try {
-            self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
-            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            self::$db = new \PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+            self::$db->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
             echo "Connected successfully to db <br><br>";
         }
-        catch (PDOException $e) {
+        catch (\PDOException $e) {
             echo "Connection Error: " . $e->getMessage();
         }
     }
